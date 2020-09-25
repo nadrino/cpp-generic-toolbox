c89c6f3fa1700767736aa861ab020b1e3bba3aed
Adrien BLANCHET <adrien.blanchet@live.fr>
2020-09-25 10:47:54 +0200

    generalize getSortPermutation

--- include/GenericToolbox.h
+++ include/GenericToolbox.h
@@ -52,7 +52,7 @@ namespace GenericToolbox{
   //! Vector management
   template <typename T> bool doesElementIsInVector(T element_, const std::vector<T>& vector_);
   bool doesElementIsInVector(const char* element_, const std::vector<std::string>& vector_);
-  template <typename T> std::vector<size_t> getSortPermutation(const std::vector<T>& vectorToSort_, std::function<bool(T&, T&)> compareLambda_ );
+  template <typename T, typename Compare> std::vector<size_t> getSortPermutation(const std::vector<T>& vectorToSort_, Compare& compareLambda_ );
   template <typename T> std::vector<T> applyPermutation(const std::vector<T>& vectorToPermute_, const std::vector<std::size_t>& sortPermutation_ );
 
 
