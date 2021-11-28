38a19ad5eab56573fb8018314d5d98708378fb4c
Adrien BLANCHET <adrien.blanchet@live.fr>
2021-11-28 18:53:24 +0100

    Adding Switch extension

---
+++ include/implementation/GenericToolbox.Switch.impl.h
@@ -0,0 +1,19 @@
+//
+// Created by Nadrino on 01/09/2020.
+//
+
+#ifndef CPP_GENERIC_TOOLBOX_GENERICTOOLBOX_SWITCH_IMPL_H
+#define CPP_GENERIC_TOOLBOX_GENERICTOOLBOX_SWITCH_IMPL_H
+
+#ifdef __SWITCH__
+
+namespace GenericToolbox {
+
+  
+
+}
+
+#endif
+
+
+#endif // CPP_GENERIC_TOOLBOX_GENERICTOOLBOX_SWITCH_IMPL_H
