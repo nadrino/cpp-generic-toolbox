d294d3a818ada30b719da8c88fc2c252e66fa248
Adrien Blanchet <adrien.blanchet@live.fr>
2021-12-03 13:36:59 +0100

    - Adding atomic wrapper class to support copy ctor

---
+++ include/GenericToolbox.Wrappers.h
@@ -0,0 +1,29 @@
+//
+// Created by Nadrino on 03/12/2021.
+//
+
+#ifndef CPP_GENERIC_TOOLBOX_GENERICTOOLBOX_WRAPPERS_H
+#define CPP_GENERIC_TOOLBOX_GENERICTOOLBOX_WRAPPERS_H
+
+#include "atomic"
+
+namespace GenericToolbox{
+
+  template <typename T>
+  struct AtomicWrapper
+  {
+    std::atomic<T> atomicValue;
+
+    AtomicWrapper() : atomicValue() {}
+    explicit AtomicWrapper(const std::atomic<T> &a) : atomicValue(a.load()){}
+    AtomicWrapper(const AtomicWrapper &other): atomicValue(other.atomicValue.load()){}
+    AtomicWrapper &operator=(const AtomicWrapper &other){
+      atomicValue.store(other.atomicValue.load());
+    }
+
+  };
+
+}
+
+
+#endif //CPP_GENERIC_TOOLBOX_GENERICTOOLBOX_WRAPPERS_H
