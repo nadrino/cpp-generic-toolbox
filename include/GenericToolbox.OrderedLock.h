6d7f9a571ab495c5bf98ff0a7427ae9130235ba0
Adrien Blanchet <adrien.blanchet@live.fr>
2022-02-01 14:28:30 +0100

    Adding ordered lock

---
+++ include/GenericToolbox.OrderedLock.h
@@ -0,0 +1,33 @@
+//
+// Created by Adrien BLANCHET on 01/02/2022.
+//
+
+#ifndef CPP_GENERIC_TOOLBOX_GENERICTOOLBOX_ORDEREDLOCK_H
+#define CPP_GENERIC_TOOLBOX_GENERICTOOLBOX_ORDEREDLOCK_H
+
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+
+
+namespace GenericToolbox{
+
+  class OrderedLock {
+
+  public:
+    inline OrderedLock();
+    inline void lock();
+    inline void unlock();
+    inline bool isLocked();
+
+  private:
+    bool _isLocked_{false};
+    std::mutex _mutexLock_{};
+    std::queue<std::condition_variable *> _conditionVariable_{};
+  };
+
+}
+
+#include "implementation/GenericToolbox.OrederedLock.impl.h"
+
+#endif //CPP_GENERIC_TOOLBOX_GENERICTOOLBOX_ORDEREDLOCK_H
---
+++ include/implementation/GenericToolbox.OrederedLock.impl.h
@@ -0,0 +1,41 @@
+//
+// Created by Adrien BLANCHET on 01/02/2022.
+//
+
+#ifndef CPP_GENERIC_TOOLBOX_GENERICTOOLBOX_OREDEREDLOCK_IMPL_H
+#define CPP_GENERIC_TOOLBOX_GENERICTOOLBOX_OREDEREDLOCK_IMPL_H
+
+#include "GenericToolbox.OrderedLock.h"
+
+namespace GenericToolbox{
+
+  OrderedLock::OrderedLock() = default;
+  void OrderedLock::lock() {
+    std::unique_lock<std::mutex> acquire(_mutexLock_);
+    if (_isLocked_) {
+      std::condition_variable signal{};
+      _conditionVariable_.emplace(&signal);
+      signal.wait(acquire);
+    }
+    else {
+      _isLocked_ = true;
+    }
+  }
+  void OrderedLock::unlock() {
+    std::unique_lock<std::mutex> acquire(_mutexLock_);
+    if (_conditionVariable_.empty()) {
+      _isLocked_ = false;
+    }
+    else {
+      _conditionVariable_.front()->notify_one();
+      _conditionVariable_.pop();
+    }
+  }
+
+  bool OrderedLock::isLocked() {
+    return _isLocked_;
+  }
+
+}
+
+#endif //CPP_GENERIC_TOOLBOX_GENERICTOOLBOX_OREDEREDLOCK_IMPL_H
