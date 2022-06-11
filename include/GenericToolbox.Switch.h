8f661ce30e24feabd45842c048e30c1864f7696d
Adrien Blanchet <adrien.blanchet@live.fr>
2022-06-11 13:20:58 +0200

    - qdding switch functions

--- include/GenericToolbox.Switch.h
+++ include/GenericToolbox.Switch.h
@@ -7,9 +7,33 @@
 
 #ifdef __SWITCH__
 
+#include "switch.h"
+
 namespace GenericToolbox::Switch{
 
+  // IO
+  namespace IO{
 
+    struct Cache{
+      Cache() = default;
+
+      inline void initFileSystemBuffer(){
+        if( this->fsBuffer != nullptr ){ return; }
+        fsdevMountSdmc();
+        this->fsBuffer = fsdevGetDeviceFileSystem("sdmc");
+      }
+
+      FsFileSystem* fsBuffer{nullptr};
+
+
+    };
+    static Cache c;
+
+    static inline void initFileSystemBuffer();
+
+    static inline void doPathIsFile(const std::string& path_);
+    static inline void deleteFile(const std::string& filePath_);
+  }
 
 }
 
--- include/implementation/GenericToolbox.Switch.impl.h
+++ include/implementation/GenericToolbox.Switch.impl.h
@@ -8,9 +8,29 @@
 #ifdef __SWITCH__
 
 namespace GenericToolbox {
+  namespace Switch{
 
-  
+    namespace IO{
 
+      static inline bool doPathIsFile(const std::string& path_){
+        bool result{false};
+        FsFile fs_FileBuffer;
+        char fs_pathBuffer[FS_MAX_PATH];
+
+        snprintf(fs_pathBuffer, FS_MAX_PATH, "%s", path_.c_str());
+        if(R_SUCCEEDED(fsFsOpenFile(c.fsBuffer, &fs_pathBuffer[0], FsOpenMode_Read, &fs_FileBuffer))) {
+          result = true;
+        }
+        fsFileClose(&fs_FileBuffer);
+        return result;
+      }
+      static inline void deleteFile(const std::string& filePath_){
+        fsFsDeleteFile(c.fsBuffer, filePath_.c_str());
+      }
+
+    }
+
+  }
 }
 
 #endif
