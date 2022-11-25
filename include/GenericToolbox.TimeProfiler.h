97eadbad1acaeb4cc802cb100441ceee2c501104
Adrien Blanchet <adrien.blanchet@live.fr>
2022-11-25 22:20:52 +0100

    adding draft time profiler

---
+++ include/GenericToolbox.TimeProfiler.h
@@ -0,0 +1,43 @@
+//
+// Created by Adrien BLANCHET on 31/10/2022.
+//
+
+#ifndef GUNDAM_GENERICTOOLBOX_TIMEPROFILER_H
+#define GUNDAM_GENERICTOOLBOX_TIMEPROFILER_H
+
+#include "GenericToolbox.h"
+
+#include "chrono"
+
+namespace GenericToolbox{
+
+  class TimeProfileMaster{
+  public:
+    TimeProfileMaster() = default;
+
+    void addDuration(const std::chrono::duration<std::chrono::steady_clock>& deltaT_){
+      totalDuration += deltaT_;
+    }
+
+  private:
+    std::chrono::duration<std::chrono::steady_clock> totalDuration;
+  };
+
+  class TimeProfileSlave{
+  public:
+    explicit TimeProfileSlave(TimeProfileMaster& master_): _master_(&master_), _start_(std::chrono::high_resolution_clock::now()) {}
+    ~TimeProfileSlave(){
+      _master_->addDuration(std::chrono::high_resolution_clock::now() - _start_);
+    }
+
+
+  private:
+    TimeProfileMaster* _master_;
+    std::chrono::time_point<std::chrono::high_resolution_clock> _start_;
+  };
+
+}
+
+
+
+#endif //GUNDAM_GENERICTOOLBOX_TIMEPROFILER_H
