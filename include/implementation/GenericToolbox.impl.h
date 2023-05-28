f92e2b76652cb3c3bef314510777d89715377a29
Adrien Blanchet <adrien.blanchet@live.fr>
2023-05-28 18:13:17 +0200

    const iterator begin and end were not introduced

--- include/implementation/GenericToolbox.impl.h
+++ include/implementation/GenericToolbox.impl.h
@@ -204,7 +204,11 @@ namespace GenericToolbox {
     }
     if( vector_.empty() ){ vector_ = vectorToInsert_; return; }
     if( vectorToInsert_.empty() ){ return; }
+#if defined(__GNUC__) && !defined(__clang__) && (__GNUC__ <= 4)
+    vector_.insert( vector_.begin() + insertBeforeThisIndex_, vectorToInsert_.begin(), vectorToInsert_.end() );
+#else
     vector_.insert( vector_.cbegin() + insertBeforeThisIndex_, vectorToInsert_.cbegin(), vectorToInsert_.cend() );
+#endif
   }
   template<typename T> inline static void insertInVector(std::vector<T> &vector_, const T &elementToInsert_, size_t insertBeforeThisIndex_){
     insertInVector(vector_, {elementToInsert_}, insertBeforeThisIndex_);
