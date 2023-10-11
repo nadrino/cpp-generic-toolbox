bf31fa3e88b91d0e48b05d39193c220ea4e27fc3
Adrien Blanchet <adrien.blanchet@live.fr>
2023-10-11 12:35:33 +0200

    simplifying ParallelWorker

--- include/GenericToolbox.ParallelWorker.h
+++ include/GenericToolbox.ParallelWorker.h
@@ -11,38 +11,64 @@
 #include <map>
 #include <mutex>
 #include <future>
+#include <utility>
 #include <vector>
 #include <string>
+#include <memory>
 
 
 // Classes : ParallelWorker
 namespace GenericToolbox{
 
   ENUM_EXPANDER(
+      WorkerState, 0,
+      Unset,
+      Set
+  );
+
+  ENUM_EXPANDER(
       ThreadStatus, 0,
       Stopped,
       Idle,
       Running
-  )
+  );
+
+  struct ThreadEntry{
+    std::shared_ptr<std::future<void>> thread{nullptr};
+    ThreadStatus threadStatus{ThreadStatus::Stopped};
+  };
+
+  struct JobEntry{
+    std::string name{};
+    std::function<void(int)> function{};
+    std::function<void()> functionPreParallel{};
+    std::function<void()> functionPostParallel{};
+    std::vector<bool> jobRequestedThreadList{false};
+
+    JobEntry() = delete;
+    explicit JobEntry(std::string  name_) : name(std::move(name_)) {}
+  };
 
   class ParallelWorker {
 
   public:
-    inline ParallelWorker();
-    inline virtual ~ParallelWorker();
+    inline ParallelWorker() = default;
+    inline virtual ~ParallelWorker(){ if( _workerState_ == WorkerState::Set ){ this->stopThreads(); } };
 
-    inline void reset();
+    inline void setIsVerbose(bool isVerbose){ _isVerbose_ = isVerbose; }
+    inline void setCheckHardwareCurrency(bool checkHardwareCurrency){ _checkHardwareCurrency_ = checkHardwareCurrency; }
+    inline void setNThreads(int nThreads){ _nThreads_ = nThreads; }
+    inline void setCpuTimeSaverIsEnabled(bool cpuTimeSaverIsEnabled){ _cpuTimeSaverIsEnabled_ = cpuTimeSaverIsEnabled; }
 
-    inline void setIsVerbose(bool isVerbose);
-    inline void setCheckHardwareCurrency(bool checkHardwareCurrency);
-    inline void setNThreads(int nThreads);
-    inline void setCpuTimeSaverIsEnabled(bool cpuTimeSaverIsEnabled);
+    // const getters
+    inline int getNThreads() const{ return _nThreads_; }
+    inline int getJobIdx(const std::string& name_) const;
+    inline const JobEntry* getJobPtr(const std::string& name_) const;
 
-    inline void initialize();
+    // getters
+    inline JobEntry* getJobPtr(const std::string& name_);
 
-    inline const std::vector<std::string> &getJobNameList() const;
-    inline int getNThreads() const;
-
+    // core
     inline void addJob(const std::string& jobName_, const std::function<void(int)>& function_); // int arg is supposed to be the thread id
     inline void setPostParallelJob(const std::string& jobName_, const std::function<void()>& function_);
     inline void setPreParallelJob(const std::string& jobName_, const std::function<void()>& function_);
@@ -50,31 +76,29 @@ namespace GenericToolbox{
     inline void removeJob(const std::string& jobName_);
 
     inline void pauseParallelThreads();
-    inline void unPauseParallelThreads();
+    inline void unPauseParallelThreads(){ _pauseThreads_ = false; }
 
   protected:
     inline void reStartThreads();
     inline void startThreads();
     inline void stopThreads();
 
+    inline void threadWorker(int iThread_);
+
   private:
     // Parameters
     bool _isVerbose_{true};
     bool _checkHardwareCurrency_{true};
     bool _cpuTimeSaverIsEnabled_{false};
-    int _nThreads_{-1};
+    int _nThreads_{1};
 
     // Internals
-    bool _isInitialized_{false};
     bool _stopThreads_{false};
     bool _pauseThreads_{false};
-    std::vector<std::future<void>> _threadsList_;
-    std::vector<ThreadStatus> _threadStatusList_;
-    std::vector<std::function<void(int)>> _jobFunctionList_;
-    std::vector<std::function<void()>> _jobFunctionPostParallelList_;
-    std::vector<std::function<void()>> _jobFunctionPreParallelList_;
-    std::vector<std::string> _jobNameList_;
-    std::vector<std::vector<bool>> _jobTriggerList_;
+
+    WorkerState _workerState_{WorkerState::Unset};
+    std::vector<ThreadEntry> _threadsList_{};
+    std::vector<JobEntry> _jobEntryList_{};
 
   };
 
--- include/implementation/GenericToolbox.ParallelWorker.impl.h
+++ include/implementation/GenericToolbox.ParallelWorker.impl.h
@@ -5,196 +5,146 @@
 #ifndef CPP_GENERIC_TOOLBOX_GENERICTOOLBOX_THREADPOOL_IMPL_H
 #define CPP_GENERIC_TOOLBOX_GENERICTOOLBOX_THREADPOOL_IMPL_H
 
-#include "../GenericToolbox.h"
+#include "GenericToolbox.h"
+
 
 // Classes: ParallelWorker
 namespace GenericToolbox{
 
-  inline ParallelWorker::ParallelWorker(){
-    this->reset();
+  // const getters
+  inline int ParallelWorker::getJobIdx(const std::string& name_) const{
+    return GenericToolbox::findElementIndex(name_, _jobEntryList_, [](const JobEntry& job){ return job.name; });
   }
-  inline ParallelWorker::~ParallelWorker() {
-    std::cout << "Stopping ParallelWorkers..." << std::endl;
-    stopThreads();
-  }
-
-  inline void ParallelWorker::reset() {
-    _isInitialized_ = false;
-    _isVerbose_ = false;
-    _checkHardwareCurrency_ = false;
-    _nThreads_ = -1;
-
-    stopThreads();
-    _jobNameList_.clear();
-    _jobTriggerList_.clear();
-    _jobFunctionList_.clear();
-    _jobFunctionPostParallelList_.clear();
-    _jobFunctionPreParallelList_.clear();
+  inline const JobEntry* ParallelWorker::getJobPtr(const std::string& name_) const {
+    auto idx = this->getJobIdx( name_ );
+    if( idx == -1 ){ return nullptr; }
+    return &_jobEntryList_[idx];
   }
 
-  inline void ParallelWorker::setIsVerbose(bool isVerbose) {
-    _isVerbose_ = isVerbose;
-  }
-  inline void ParallelWorker::setCheckHardwareCurrency(bool checkHardwareCurrency) {
-    _checkHardwareCurrency_ = checkHardwareCurrency;
-  }
-  inline void ParallelWorker::setNThreads(int nThreads) {
-    if(_isInitialized_){
-      throw std::logic_error("Can't set the number of threads while already initialized.");
-    }
-    if( _checkHardwareCurrency_ and nThreads > std::thread::hardware_concurrency() ){
-      std::cout << GET_VAR_NAME_VALUE(std::thread::hardware_concurrency()) << std::endl;
-      throw std::logic_error("Too much threads wrt your hardware.");
-    }
-    _nThreads_ = nThreads;
-  }
-  inline void ParallelWorker::setCpuTimeSaverIsEnabled(bool cpuTimeSaverIsEnabled) {
-    if(cpuTimeSaverIsEnabled != _cpuTimeSaverIsEnabled_){
-      if(cpuTimeSaverIsEnabled) this->stopThreads();
-      if(not cpuTimeSaverIsEnabled and not _jobNameList_.empty()) this->startThreads();
-    }
-    _cpuTimeSaverIsEnabled_ = cpuTimeSaverIsEnabled;
-  }
-
-  inline void ParallelWorker::initialize() {
-    if( _nThreads_ < 1 ){
-      throw std::logic_error("_nThreads_ should be >= 1");
-    }
-    _isInitialized_ = true;
+  // getters
+  inline JobEntry* ParallelWorker::getJobPtr(const std::string& name_){
+    return const_cast<JobEntry*>(const_cast<const ParallelWorker*>(this)->getJobPtr(name_));
   }
 
   inline void ParallelWorker::addJob(const std::string &jobName_, const std::function<void(int)> &function_) {
-    if( not _isInitialized_ ){
-      throw std::logic_error("Can't add job while not initialized");
-    }
-    if( GenericToolbox::doesElementIsInVector(jobName_, _jobNameList_) ){
-      throw std::logic_error("A job with the same name has already been added: " + jobName_);
-    }
-    if( not function_ ){ // is it callable?
-      throw std::logic_error("the provided function is not callable");
-    }
+    // is it callable?
+    if( not function_ ){ throw std::logic_error("the provided function is not callable"); }
 
+    // existing job?
+    auto* jobPtr{ this->getJobPtr(jobName_) };
+    if( jobPtr != nullptr ){ throw std::logic_error("A job with the same name has already been added: " + jobName_); }
+
+    // make sure we don't loop over the job entries
     this->pauseParallelThreads();
-    _jobNameList_.emplace_back(jobName_);
-    _jobFunctionList_.emplace_back(function_);
-    _jobFunctionPostParallelList_.emplace_back();
-    _jobFunctionPreParallelList_.emplace_back();
-    _jobTriggerList_.emplace_back(std::vector<bool>(_nThreads_, false));
+
+    // now emplace_back()
+    _jobEntryList_.emplace_back( jobName_ );
+    jobPtr = this->getJobPtr( jobName_ );
+    jobPtr->function = function_;
+    jobPtr->jobRequestedThreadList.resize(_nThreads_, false);
+
+    // let's allow the job loop again
     this->unPauseParallelThreads();
 
-    if( not _cpuTimeSaverIsEnabled_ and _threadsList_.empty() ){
+    if( not _cpuTimeSaverIsEnabled_ and _workerState_ == Unset ){
       // start the parallel threads if not already
-      startThreads();
+      this->startThreads();
     }
   }
   inline void ParallelWorker::setPostParallelJob(const std::string& jobName_, const std::function<void()>& function_){
-    if( not _isInitialized_ ){
-      throw std::logic_error("Can't add post parallel job while not initialized");
-    }
-    int jobIndex = GenericToolbox::findElementIndex(jobName_, _jobNameList_);
-    if( jobIndex == -1 ){
-      throw std::logic_error(jobName_ + ": is not in the available jobsList");
-    }
-    if( not function_ ){ // is it callable?
-      throw std::logic_error("the provided post parallel function is not callable");
-    }
+    // is it callable?
+    if( not function_ ){ throw std::logic_error("the provided function is not callable"); }
 
+    // existing job?
+    auto* jobPtr{ this->getJobPtr(jobName_) };
+    if( jobPtr == nullptr ){ throw std::logic_error(jobName_ + ": is not in the available jobsList"); }
+
+    // make sure we don't loop over the job entries
     this->pauseParallelThreads();
-    _jobFunctionPostParallelList_.at(jobIndex) = function_;
+
+    jobPtr->functionPostParallel = function_;
+
+    // let's allow the job loop again
     this->unPauseParallelThreads();
   }
   inline void ParallelWorker::setPreParallelJob(const std::string& jobName_, const std::function<void()>& function_){
-    if( not _isInitialized_ ){
-      throw std::logic_error("Can't add post parallel job while not initialized");
-    }
-    int jobIndex = GenericToolbox::findElementIndex(jobName_, _jobNameList_);
-    if( jobIndex == -1 ){
-      throw std::logic_error(jobName_ + ": is not in the available jobsList");
-    }
-    if( not function_ ){ // is it callable?
-      throw std::logic_error("the provided post parallel function is not callable");
-    }
+    // is it callable?
+    if( not function_ ){ throw std::logic_error("the provided function is not callable"); }
 
+    // existing job?
+    auto* jobPtr{ this->getJobPtr(jobName_) };
+    if( jobPtr == nullptr ){ throw std::logic_error(jobName_ + ": is not in the available jobsList"); }
+
+    // make sure we don't loop over the job entries
     this->pauseParallelThreads();
-    _jobFunctionPreParallelList_.at(jobIndex) = function_;
+
+    jobPtr->functionPreParallel = function_;
+
+    // let's allow the job loop again
     this->unPauseParallelThreads();
   }
   inline void ParallelWorker::runJob(const std::string &jobName_) {
-    if( _isVerbose_ ) std::cout << "Running \"" << jobName_ << "\" on " << _nThreads_ << " parallel threads..." << std::endl;
-    if( not _isInitialized_ ){
-      throw std::logic_error("Can't run job while not initialized");
-    }
-    int jobIndex = GenericToolbox::findElementIndex(jobName_, _jobNameList_);
-    if( jobIndex == -1 ){
-      throw std::logic_error(jobName_ + ": is not in the available jobsList");
+    if( _isVerbose_ ){ std::cout << "Running \"" << jobName_ << "\" on " << _nThreads_ << " parallel threads..." << std::endl; }
+
+    // existing job?
+    auto* jobPtr{ this->getJobPtr(jobName_) };
+    if( jobPtr == nullptr ){ throw std::logic_error(jobName_ + ": is not in the available jobsList"); }
+
+    // runs the pre-job if set
+    if( jobPtr->functionPreParallel ){ jobPtr->functionPreParallel(); }
+
+    // request the jobs
+    for( int iThread = 0 ; iThread < _nThreads_ ; iThread++ ){
+      jobPtr->jobRequestedThreadList[ iThread ] = true;
     }
 
-    if( _jobFunctionPreParallelList_.at(jobIndex) ){ // is it callable?
-      _jobFunctionPreParallelList_.at(jobIndex)();
-    }
+    // launch threads if not already
+    if( _workerState_ == Unset ){ this->startThreads(); }
 
-    if(_cpuTimeSaverIsEnabled_) this->startThreads();
-    for( int iThread = 0 ; iThread < _nThreads_-1 ; iThread++ ){
-      _jobTriggerList_.at(jobIndex).at(iThread) = true;
-    }
-
-    _jobFunctionList_.at(jobIndex)(_nThreads_-1); // do the last job in the main thread
+    // do the last job in the main thread
+    jobPtr->function(_nThreads_-1);
+    jobPtr->jobRequestedThreadList.back() = false; // flag as done
 
     for( int iThread = 0 ; iThread < _nThreads_-1 ; iThread++ ){
       if( _isVerbose_ ) std::cout << "Waiting for thread #" << iThread << " to be finish the job..." << std::endl;
-      while( _jobTriggerList_.at(jobIndex).at(iThread) ) std::this_thread::sleep_for( std::chrono::microseconds(100) ); // wait
+      while( jobPtr->jobRequestedThreadList[iThread] ) std::this_thread::sleep_for( std::chrono::microseconds(100) ); // wait
     }
-    if(_cpuTimeSaverIsEnabled_) this->stopThreads();
 
-    if( _jobFunctionPostParallelList_.at(jobIndex) ){ // is it callable?
-      _jobFunctionPostParallelList_.at(jobIndex)();
-    }
+    // CPU time saver stops the parallel threads when no job is requested
+    if( _cpuTimeSaverIsEnabled_ ){ this->stopThreads(); }
+
+    // runs the post-job if set
+    if( jobPtr->functionPostParallel ){ jobPtr->functionPostParallel(); }
 
   }
   inline void ParallelWorker::removeJob(const std::string& jobName_){
-    if( not _isInitialized_ ){
-      throw std::logic_error("Can't run job while not initialized");
-    }
-    int jobIndex = GenericToolbox::findElementIndex(jobName_, _jobNameList_);
-    if( jobIndex == -1 ){
-      throw std::logic_error(jobName_ + ": is not in the available jobsList");
-    }
+    // existing job?
+    auto jobIdx{ this->getJobIdx(jobName_) };
+    if( jobIdx == -1 ){ throw std::logic_error(jobName_ + ": is not in the available jobsList"); }
 
+    // make sure we don't loop over the job entries
     this->pauseParallelThreads();
-    _jobNameList_.erase(_jobNameList_.begin() + jobIndex);
-    _jobFunctionList_.erase(_jobFunctionList_.begin() + jobIndex);
-    _jobFunctionPostParallelList_.erase(_jobFunctionPostParallelList_.begin() + jobIndex);
-    _jobFunctionPreParallelList_.erase(_jobFunctionPreParallelList_.begin() + jobIndex);
-    _jobTriggerList_.erase(_jobTriggerList_.begin() + jobIndex);
+
+    _jobEntryList_.erase(_jobEntryList_.begin() + jobIdx);
+
+    // let's allow the job loop again
     this->unPauseParallelThreads();
 
-    if( _jobNameList_.empty() ){
-      // stop the parallel threads if no job is in the pool
-      stopThreads();
-    }
+    // stop the parallel threads if no job is in the pool
+    if( _jobEntryList_.empty() ){ this->stopThreads(); }
   }
 
   inline void ParallelWorker::pauseParallelThreads(){
-    _pauseThreads_ = true; // prevent the threads to loop over the available jobs
-    for( const auto& threadTriggers : _jobTriggerList_ ){
+    // prevent the threads to loop over the available jobs
+    _pauseThreads_ = true;
+
+    // wait for every thread to finish its current job
+    for( auto& job : _jobEntryList_ ){
       for( int iThread = 0 ; iThread < _nThreads_-1 ; iThread++ ){
         if( _isVerbose_ ) std::cout << "Waiting for thread #" << iThread << " to be on paused state..." << std::endl;
-        while( threadTriggers.at(iThread) ) std::this_thread::sleep_for( std::chrono::microseconds(100) ); // wait for every thread to finish its current job
+        while( job.jobRequestedThreadList[iThread] ) std::this_thread::sleep_for( std::chrono::microseconds(100) );
       }
     }
-  }
-  inline void ParallelWorker::unPauseParallelThreads(){
-//    std::unique_lock<std::mutex> lock(_workerMutex_);
-    _pauseThreads_ = false;
-//    _conditionVariable_.notify_all();
-//    lock.unlock();
-  }
-
-  inline const std::vector<std::string> &ParallelWorker::getJobNameList() const {
-    return _jobNameList_;
-  }
-  inline int ParallelWorker::getNThreads() const {
-    return _nThreads_;
   }
 
   inline void ParallelWorker::reStartThreads() {
@@ -205,56 +155,59 @@ namespace GenericToolbox{
     _stopThreads_ = false;
     unPauseParallelThreads(); // make sure
 
+    _threadsList_.clear();
+    _threadsList_.reserve(_nThreads_);
     for( int iThread = 0 ; iThread < _nThreads_-1 ; iThread++ ){
-
-      std::function<void()> asyncLoop = [this, iThread](){
-        size_t jobIndex;
-        while( not _stopThreads_ ){
-
-          std::this_thread::sleep_for( std::chrono::microseconds(100) ); // let space for other threads...
-          while( _pauseThreads_ ) std::this_thread::sleep_for( std::chrono::microseconds(100) ); // wait
-
-          // dev
-//          if( _pauseThreads_.atomicValue ){
-//            std::unique_lock<std::mutex> lock(_workerMutex_);
-//            _conditionVariable_.wait(lock, [&](){ return !_pauseThreads_.atomicValue; });
-//            lock.unlock();
-//          }
-
-          if( _stopThreads_ ) break; // if stop requested while in pause
-          _threadStatusList_.at(iThread) = ThreadStatus::Idle;
-
-          for( jobIndex = 0 ; jobIndex < _jobTriggerList_.size() ; jobIndex++ ){
-            if( _pauseThreads_ ) break; // jump out!
-            if( _jobTriggerList_[jobIndex][iThread] ){ // is it triggered?
-              _threadStatusList_.at(iThread) = ThreadStatus::Running;
-              _jobFunctionList_.at(jobIndex)(iThread); // run
-              _jobTriggerList_[jobIndex][iThread] = false; // un-trigger this thread
-            }
-          } // jobIndex
-          _threadStatusList_.at(iThread) = ThreadStatus::Idle;
-
-        } // not stop
-        _threadStatusList_.at(iThread) = ThreadStatus::Stopped;
-      };
-
-      _threadStatusList_.emplace_back(ThreadStatus::Stopped);
-      _threadsList_.emplace_back( std::async( std::launch::async, asyncLoop ) );
-
+      _threadsList_.emplace_back();
+      _threadsList_.back().thread = std::make_shared<std::future<void>>(
+          std::async( std::launch::async, [this, iThread]{ this->threadWorker( iThread ); } )
+      );
     }
 
-    for( int iThread = 0 ; iThread < _nThreads_-1 ; iThread++ ){
-      while( _threadStatusList_.at(iThread) == ThreadStatus::Stopped ) std::this_thread::sleep_for( std::chrono::microseconds(100) ); // wait to be in Idle state
+    // waiting for all of them to start
+    for( auto& thread : _threadsList_ ){
+      while( thread.threadStatus == ThreadStatus::Stopped ){ std::this_thread::sleep_for( std::chrono::microseconds(100) ); }
     }
+
+    _workerState_ = WorkerState::Set;
   }
   inline void ParallelWorker::stopThreads(){
     _stopThreads_ = true;
     this->unPauseParallelThreads(); // is the threads were on pause state
-    for( int iThread = 0 ; iThread < int(_threadsList_.size()) ; iThread++ ){
-      _threadsList_.at(iThread).get();
-    }
+
+    for( auto& thread : _threadsList_ ){ thread.thread.get(); }
+
     _threadsList_.clear();
-    _threadStatusList_.clear();
+    _workerState_ = WorkerState::Unset;
+  }
+
+
+  inline void ParallelWorker::threadWorker(int iThread_){
+
+    auto* thisWorker{&_threadsList_[iThread_]};
+
+    size_t jobIndex;
+    while( not _stopThreads_ ){
+      thisWorker->threadStatus = ThreadStatus::Idle;
+
+      std::this_thread::sleep_for( std::chrono::microseconds(100) ); // let space for other threads...
+      while( _pauseThreads_ ) std::this_thread::sleep_for( std::chrono::microseconds(100) ); // wait
+
+      if( _stopThreads_ ) break; // if stop requested while in pause
+
+      for( auto& job : _jobEntryList_ ){
+        if( _pauseThreads_ ){ break; } // jump out!
+        if( job.jobRequestedThreadList[iThread_] ){
+          thisWorker->threadStatus = ThreadStatus::Running;
+          job.function(iThread_); // run
+          thisWorker->threadStatus = ThreadStatus::Idle;
+          job.jobRequestedThreadList[iThread_] = false; // this thread has done its job
+        }
+      }
+
+    } // not stop
+
+    thisWorker->threadStatus = ThreadStatus::Stopped;
   }
 
 
